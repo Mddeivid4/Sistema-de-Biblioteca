9a6b48d073fcc23e1151fd2be71917ba0e16ca47
Deivid Martis <mddeivid54@gmail.com>
2026-06-28 15:55:21 -0300

    Adicionado metodos das classes de usuario

--- src/model/UsuarioComum.java
+++ src/model/UsuarioComum.java
@@ -1,5 +1,22 @@
 package model;
 
-public class UsuarioComum {
+import interfaces.Notificavel;
 
+public class UsuarioComum extends Usuario implements Notificavel{
+	
+	public UsuarioComum(String nome, int id) {
+		super.nome = nome;
+		super.id = id;
+	}
+	
+	@Override
+	public int getLimiteEmprestimo() {
+		return 3;
+	}
+
+	@Override
+	public void enviarNotificacao(String mensagem) {
+		System.out.println(mensagem);
+		
+	}
 }
--- src/model/UsuarioPremium.java
+++ src/model/UsuarioPremium.java
@@ -1,5 +1,22 @@
 package model;
 
-public class UsuarioPremium {
+import interfaces.Notificavel;
 
+public class UsuarioPremium extends Usuario implements Notificavel{
+	
+	public UsuarioPremium(String nome, int id) {
+		super.nome = nome;
+		super.id = id;
+	}
+	
+	@Override
+	public int getLimiteEmprestimo() {
+		return 5;
+	}
+
+	@Override
+	public void enviarNotificacao(String mensagem) {
+		System.out.println(mensagem);
+	}
+	
 }
