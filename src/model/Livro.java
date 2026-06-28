5ac9da8f53db09eed11c8c28fee7bddc64feef5f
Deivid Martis <mddeivid54@gmail.com>
2026-06-28 14:34:57 -0300

    Adicionado metodos na classe Livro

--- src/model/Livro.java
+++ src/model/Livro.java
@@ -1,5 +1,42 @@
 package model;
 
 public class Livro {
+	private String titulo;
+	private String autor;
+	private int quantidade;
+	
+	public String getTitulo() {
+		return titulo;
+	}
 
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	//Adicionar Exception para caso não tenha quantidade disponivel
+	public void Emprestar(String titulo) {
+		if(quantidade > 0) {
+			quantidade -= 1;
+		}
+	}
+	
+	public void Devolver(String titulo) {
+		quantidade += 1;
+	}
 }
